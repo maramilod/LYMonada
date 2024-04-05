6f19fb4c3c009d00fb5436dcf0fa095f625ff4ee
lujaiiin <132622956+lujaiiin@users.noreply.github.com>
2024-04-05 08:53:57 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// Import the React library, which is necessary for creating React components
+import React from 'react';
 
+// Import the ReactDOM library, which is used to render React components to the DOM
+import ReactDOM from 'react-dom/client';
+
+// Import the App component from the App.jsx file. This is the main component of your application.
+import App from './App.jsx';
+
+// Import the CSS file for your application. This file contains global styles that apply to your entire application.
+import './index.css';
+
+// Use ReactDOM to render the App component wrapped in React.StrictMode into the DOM element with the id 'root'.
+// React.StrictMode is a wrapper component that checks for potential problems in the application during the development build.
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+ </React.StrictMode>,
+);
