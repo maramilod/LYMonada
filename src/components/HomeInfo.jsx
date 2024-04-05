392ccc3daf9c88abde228370c5f047cf08cbd555
lujaiiin <132622956+lujaiiin@users.noreply.github.com>
2024-04-05 09:16:07 +0200

    Update HomeInfo.jsx

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,130 +1,83 @@
+import React from 'react';
 import { Link } from "react-router-dom";
-import { facebook, linkedin } from "../assets/icons";
-import { github } from "../assets/icons";
+import { facebook, linkedin, github } from "../assets/icons";
 
+// Function to render social icons
+const renderSocialIcons = () => (
+ <div className="social-icons">
+    <Link to='/lujain'>
+      <img src={facebook} alt='facebook' className='w-8 h-8 cursor-pointer' />
+    </Link>
+    <Link to='/lujain'>
+      <img src={github} alt='github' className='w-8 h-8 cursor-pointer' />
+    </Link>
+    <Link to='/lujain'>
+      <img src={linkedin} alt='linkedin' className='w-8 h-8 cursor-pointer' />
+    </Link>
+ </div>
+);
 
+// Function to render the main content based on the current stage
 const HomeInfo = ({ currentStage }) => {
-  if (currentStage === 4)
-    return (
-        <div className='info-box'>
-        <p className='font-medium sm:text-xl text-center'>
-        Hi, I'm  <span className='font-semibold mx-2 text-white'>Lujain</span>
-        👋
-        <br />
-        A Software Engineer from Libya 🇭🇷
-        </p>
-        <div class="social-icons"><Link to='/lujain' >
- <img src={facebook} alt='jukebox' className='w-8 h-8 cursor-pointer' /> </Link><Link to='/lujain' >
- <img src={github} alt='jukebox' className='w-8 h-8 cursor-pointer' /> </Link><Link to='/lujain' >
- <img src={linkedin} alt='jukebox' className='w-8 h-8 cursor-pointer' /> </Link>
-</div>
-
-    
-        <div className='neo-brutalism-white neo-btn'>
-       
-        <Link to='/lujain' >
-
-          See more
-        
-        </Link>
-        </div>
-      </div>
-    );
-
-  if (currentStage === 5) {
-    return (
-      <div className='info-box'>
-        <p className='font-medium sm:text-xl text-center'>
-        Hi, I'm  <span className='font-semibold mx-2 text-white'>Maram</span>
-        👋
-        <br />
-        A Software Engineer from Libya 🇭🇷
-        </p>
-        <div class="social-icons"><Link to='/lujain' >
- <img src={facebook} alt='jukebox' className='w-8 h-8 cursor-pointer' /> </Link><Link to='/lujain' >
- <img src={github} alt='jukebox' className='w-8 h-8 cursor-pointer' /> </Link><Link to='/lujain' >
- <img src={linkedin} alt='jukebox' className='w-8 h-8 cursor-pointer' /> </Link>
-</div>
-
-        <div className='neo-brutalism-white neo-btn'>
-        <Link to='/maramilod' >
-
-          See more
-        
-        </Link>
-        </div>
-      </div>
-    );
-  }
-
-  if (currentStage === 3) {
-    return (
-        <div className='info-box'>
-        <p className='font-medium sm:text-xl text-center'>
-        Hi, I'm  <span className='font-semibold mx-2 text-white'>Yasmen</span>
-        👋
-        <br />
-        A Software Engineer from Libya 🇭🇷
-        </p>        <div class="social-icons"><Link to='/lujain' >
- <img src={facebook} alt='jukebox' className='w-8 h-8 cursor-pointer' /> </Link><Link to='/lujain' >
- <img src={github} alt='jukebox' className='w-8 h-8 cursor-pointer' /> </Link><Link to='/lujain' >
- <img src={linkedin} alt='jukebox' className='w-8 h-8 cursor-pointer' /> </Link>
-</div>
-
-
-        <div className='neo-brutalism-white neo-btn'>
-        <Link to='/yas' >
-          See more
-        
-        </Link>
-      
-        </div>
-      </div>
-    );
-  }
-
-
-  return null;
-};
-
-export default HomeInfo;
-/*import { Html } from '@react-three/drei';
-
-const HomeInfo = ({ currentStage }) => {
+ // Determine the content to display based on the current stage
  let content;
-
- if (currentStage === 1) {
-    content = (
-      <Html>
+ switch (currentStage) {
+    case 4:
+      content = (
         <div className='info-box'>
           <p className='font-medium sm:text-xl text-center'>
             Hi, I'm <span className='font-semibold mx-2 text-white'>Lujain</span> 👋
             <br />
             A Software Engineer from Libya 🇭🇷
           </p>
+          {renderSocialIcons()}
           <div className='neo-brutalism-white neo-btn'>
-            See more
-          
+            <Link to='/lujain'>
+              See more
+            </Link>
           </div>
         </div>
-      </Html>
-    );
- } else if (currentStage === 2) {
-    content = (
-      <Html>
-        {/* Similar content for stage 2 }
-        </Html>
-        );
-     } else if (currentStage === 3) {
-        content = (
-          <Html>
-            {/* Similar content for stage 3 }
-          </Html>
-        );
-     }
-    
-     return content;
-    };
-   
-    
-    export default HomeInfo; */
+      );
+      break;
+    case 5:
+      content = (
+        <div className='info-box'>
+          <p className='font-medium sm:text-xl text-center'>
+            Hi, I'm <span className='font-semibold mx-2 text-white'>Maram</span> 👋
+            <br />
+            A Software Engineer from Libya 🇭🇷
+          </p>
+          {renderSocialIcons()}
+          <div className='neo-brutalism-white neo-btn'>
+            <Link to='/maramilod'>
+              See more
+            </Link>
+          </div>
+        </div>
+      );
+      break;
+    case 3:
+      content = (
+        <div className='info-box'>
+          <p className='font-medium sm:text-xl text-center'>
+            Hi, I'm <span className='font-semibold mx-2 text-white'>Yasmen</span> 👋
+            <br />
+            A Software Engineer from Libya 🇭🇷
+          </p>
+          {renderSocialIcons()}
+          <div className='neo-brutalism-white neo-btn'>
+            <Link to='/yas'>
+              See more
+            </Link>
+          </div>
+        </div>
+      );
+      break;
+    default:
+      content = null;
+ }
+
+ return content;
+};
+
+export default HomeInfo;
