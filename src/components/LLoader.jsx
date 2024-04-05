953ff3fc9d8e9d89b410a2e01194e47ee4ddd472
lujaiiin <132622956+lujaiiin@users.noreply.github.com>
2024-04-05 09:17:17 +0200

    Update LLoader.jsx

--- src/components/LLoader.jsx
+++ src/components/LLoader.jsx
@@ -3,68 +3,65 @@ import { Html } from "@react-three/drei";
 
 const LLoader = () => {
  return (
-    <Html><div class="name">Hey Welcome In My Home Just <div className="color"> Knock ON It</div> And It Gonna Open To U ...enjoy ^-^</div>
-   <div class="container">
-   <div class="container2">
-         <div class="ring"></div>
-         <div class="ring"></div>
-         <div class="ring"></div>
-        
-         </div> 
-         
+    <Html>
+      {/* Welcome message */}
+      <div className="name">
+        Hey Welcome In My Home Just <div className="color"> Knock ON It</div> And It Gonna Open To U ...enjoy ^-^
       </div>
-     
+
+      {/* Container for the rotating rings */}
+      <div className="container">
+        <div className="container2">
+          {/* Three rotating rings */}
+          <div className="ring"></div>
+          <div className="ring"></div>
+          <div className="ring"></div>
+        </div>
+      </div>
+
+      {/* Styles for the component */}
       <style jsx>{`
-  .name {
-    color: #ffffff;
-    margin-left: -95px; /* Removed to center the text */
-    width: 202px; /* Ensures the span spans the full width of the container */
-    text-align: center; /* Centers the text horizontally */
- 
-   }
-   .container{
-    display: flex;
+        .name {
+          color: #ffffff;
+          text-align: center;
+          width: 202px;
+        }
+        .container {
+          display: flex;
+          margin-top: -55vh;
+          width: 200%;
+          height: 100vh;
+          margin-left: -95px;
+        }
+        .container2 {
+          position: relative;
+          display: flex;
+        }
+        .ring {
+          position: absolute;
+          width: 200px;
+          height: 200px;
+          border: 8px solid #000;
+          border-radius: 50%;
+          animation: rotate 2s linear infinite;
+        }
+        .ring:nth-child(1) {
+          border-color: rgb(242, 39, 39, 1);
+        }
+        .color {
+          color: rgb(242, 39, 39, 1);
+        }
 
-    margin-top:-55vh;
-    width: 200%;
-    height: 100hv;
-margin-left: -95px;
-   }
-   .container2{
-   position: relative;
-   display: flex;
-  }
-    .ring { 
-      --tw-ring-shadow: var(--tw-ring-inset) 0 0 0 calc(3px + var(--tw-ring-offset-width)) rgb(0 11 27 / 0%);
-      position: absolute;
-
-        width: 200px;
-        height: 200px;
-        border: 0px solid #000;;
-        border-radius: 50%;
-    }
-    .ring:nth-child(1) {
-      border-bottom-width: 8px;
-      border-color: rgb(242, 39, 39, 1);
-      animation: rotate 2s linear infinite;
-   }
-   .color {
-    color: rgb(242, 39, 39, 1);
-   }
-
-   @keyframes rotate {
-    0%
-    {
-        transform: rotateX(35deg) rotateY(55deg) rotateZ(0deg);
-    }
-    100%
-    {
-        transform: rotateX(35deg) rotateY(55deg) rotateZ(360deg);
-    }
-}
-
-   } `}</style>
- </Html>
+        @keyframes rotate {
+          0% {
+            transform: rotateX(35deg) rotateY(55deg) rotateZ(0deg);
+          }
+          100% {
+            transform: rotateX(35deg) rotateY(55deg) rotateZ(360deg);
+          }
+        }
+      `}</style>
+    </Html>
  );
 };
 
