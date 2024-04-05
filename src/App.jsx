36e9ec03f945e2cbe881faefb690f2ff10f016bc
lujaiiin <132622956+lujaiiin@users.noreply.github.com>
2024-04-05 08:55:18 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+// Import necessary components from react-router-dom for routing
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-import  {Home, Projects ,Maramilod, Lujain} from "./pages";
-//import Navbar  from "./components/Navbar";
+// Import the Home, Projects, Maramilod, and Lujain components from the pages directory
+import { Home, Projects, Maramilod, Lujain } from "./pages";
 
+// Define the main App component
 const App = () => {
-  return (
+ return (
     <main>
+      // Use the Router component to enable routing in the application
       <Router>
-      
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/yas' element={<Projects />} />
@@ -16,7 +18,8 @@ const App = () => {
         </Routes>
       </Router>
     </main>
-  )
+ )
 }
 
+// Export the App component as the default export
 export default App;
